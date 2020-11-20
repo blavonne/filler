07d8465e92ff8aec4e5995c8cf07414e76fa761e
blavonne <luchik_sonza@mail.ru>
2020-11-20 14:08:03 +0300

    norma

--- libdraw/put_images.c
+++ libdraw/put_images.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_images.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blavonne <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/31 16:29:17 by blavonne          #+#    #+#             */
+/*   Updated: 2020/10/31 16:29:26 by blavonne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "graphics_base.h"
 
 void			put_img_to_window(t_mlx *mlx, int x, int y)
@@ -9,5 +21,3 @@ void			put_img_to_window(t_mlx *mlx, int x, int y)
 		mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->image.ptr,\
 	x, y);
 }
-
-
